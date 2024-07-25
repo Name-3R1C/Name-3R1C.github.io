03f9a0ef524e5434f115cb8f55e2ea0fa8ff7de6
ericFYMAB <eric@fymab.com>
2024-07-25 01:06:49 -0700

    fix mobile layout for experience

--- src/app/_components/experience.tsx
+++ src/app/_components/experience.tsx
@@ -9,22 +9,25 @@ const Experience = () => {
       </h1>
       <div className="relative">
         {/* Vertical Line for Timeline */}
-        <div className="absolute left-1/2 top-0 w-px h-full bg-gray-300 transform -translate-x-1/2"></div>
+        <div className="hidden sm:block absolute left-1/2 top-0 w-px h-full bg-gray-500 transform -translate-x-1/2"></div>
 
         <ul className="relative">
           {experiences.map((exp, index) => (
             <li
               key={index}
-              className={`flex items-start mb-12 ${index % 2 === 0 ? 'flex-row' : 'flex-row-reverse'} relative`}
+              className={`flex flex-col sm:flex-row items-start mb-12 ${index % 2 === 0 ? 'sm:flex-row' : 'sm:flex-row-reverse'} relative`}
             >
               {/* Time Element */}
-              <div className={`w-1/2 flex flex-col self-center ${index % 2 === 0 ? 'pr-4 items-end' : 'pl-4 items-start'} space-y-2`}>
+              <div className={`w-full sm:w-1/2 flex flex-col self-center ${index % 2 === 0 ? 'sm:pr-4 sm:items-end' : 'sm:pl-4 sm:items-start'} items-start space-y-2`}>
                 <time className="timeline-box">{exp.time}</time>
                 <h2 className="timeline-title text-xl font-bold">{exp.title}</h2>
               </div>
               
+              {/* Horizontal Line */}
+              <hr className="w-full border-t-1 bg-gray-500 my-3 sm:hidden" />
+
               {/* Job Details */}
-              <div className={`w-1/2 ${index % 2 === 0 ? 'pl-4' : 'pr-4'} flex items-center`}>
+              <div className={`w-full sm:w-1/2 ${index % 2 === 0 ? 'sm:pl-4' : 'sm:pr-4'} flex items-center`}>
                 <div className="timeline-box">
                   <h4 className="timeline-subtitle text-md md:text-xl text-slate-600 dark:text-slate-400 pb-1 font-semibold">{exp.company}</h4>
                   <p className="timeline-desc">{exp.description}</p>
@@ -39,3 +42,48 @@ const Experience = () => {
 };
 
 export default Experience;
+
+// import React from 'react';
+// import experiences from '../data/experience';
+
+// const Experience = () => {
+//   return (
+//     <section id="experience" className="my-12 px-4 pt-20 -mt-20">
+//       <h1 className="hover:text-shadow-3 m-0 font-heading text-[clamp(3rem,_10vw,_1rem)] font-black tracking-[-0.2rem] transition-all duration-300 text-center pb-4">
+//         Experience
+//       </h1>
+//       <div className="relative">
+//         {/* Vertical Line for Timeline */}
+//         <div className="hidden sm:block absolute left-1/2 top-0 w-px h-full bg-gray-500 transform -translate-x-1/2"></div>
+
+//         <ul className="relative">
+//           {experiences.map((exp, index) => (
+//             <li
+//               key={index}
+//               // className={`flex items-start mb-12 ${index % 2 === 0 ? 'flex-row' : 'flex-row-reverse'} relative`}
+//               className={`flex flex-col sm:flex-row items-start mb-12 ${index % 2 === 0 ? 'sm:flex-row' : 'sm:flex-row-reverse'} relative`}
+//             >
+//               {/* Time Element */}
+//               {/* <div className={`w-1/2 flex flex-col self-center ${index % 2 === 0 ? 'pr-4 items-end' : 'pl-4 items-start'} space-y-2`}> */}
+//               <div className={`w-full sm:w-1/2 flex flex-col self-center ${index % 2 === 0 ? 'sm:pr-4 sm:items-end' : 'sm:pl-4 sm:items-start'} items-start space-y-2`}>
+//                 <time className="timeline-box">{exp.time}</time>
+//                 <h2 className="timeline-title text-xl font-bold">{exp.title}</h2>
+//               </div>
+              
+//               {/* Job Details */}
+//               <div className={`w-1/2 ${index % 2 === 0 ? 'pl-4' : 'pr-4'} flex items-center`}>
+//                 <div className="timeline-box">
+//                   <h4 className="timeline-subtitle text-md md:text-xl text-slate-600 dark:text-slate-400 pb-1 font-semibold">{exp.company}</h4>
+//                   <p className="timeline-desc">{exp.description}</p>
+//                 </div>
+//               </div>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Experience;
+
